f5e005bae5c4e310786c25be8a789a6de282f8ac
Benjamin Pils <34171898+BenjaminPils@users.noreply.github.com>
2021-12-14 13:45:58 +0100

    Create postprocess.ts

---
+++ UpdateData/postprocess.ts
@@ -0,0 +1,13 @@
+// 1. Install necessary packages
+const r_install = Deno.run({
+    cmd: ['sudo', 'Rscript', '-e', "install.packages(c('dplyr', 'readxl', 'readr', 'lubridate', 'stringr'))"]
+});
+
+await r_install.status();
+
+// 2. Forward the execution to the R script
+const r_run = Deno.run({
+    cmd: ['Rscript', './clean.R']
+});
+
+await r_run.status();
